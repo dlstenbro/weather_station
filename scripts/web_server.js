ebe53d1cd6d37474441cddbeac356b15b921d7d7
moomoo101 <danielstenbro101@hotmail.com>
2018-12-07 22:08:39 -0600

    Changed how the scripts folder is accessed on load
    * Fixed a problem where if index.html was called, it would not find the scripts folder.
    * Added the start script that runs on the board.

--- scripts/web_server.js
+++ scripts/web_server.js
@@ -4,10 +4,25 @@ var port = "8080"
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    console.log('request was made: ' + req);
-    var myReadStream = fs.createReadStream("index.html", 'utf8');
+    console.log('request was made: ' + req.url);
+
+    // weather_station/scripts/web_server.js
+    var parent_path = __dirname + "/../"
+    if(req.url == "/")
+    {
+	var myReadStream = fs.createReadStream("index.html", 'utf8');
+    }
+    else if(req.url == "/favicon.ico")
+    {
+	var myReadStream = fs.createReadStream("favicon.ico", 'utf8');
+    }
+    else{
+	var myReadStream = fs.createReadStream(parent_path + req.url, 'utf8');
+    }
     myReadStream.pipe(res);
 })
 
 server.listen(port);
 console.log("Server started! Listening on port " + port );
+
+
---
+++ start-station.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+top_scriptdir=$(dirname $BASH_SOURCE)
+
+echo "Starting weather station Site"
+node scripts/web_server.js &
+
+#echo "Running weather station..."
+#python app/weather_station.py
