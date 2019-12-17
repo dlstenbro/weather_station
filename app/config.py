e8b3507105664dc99fb3a16c06b986b2ffea3fc5
root <root@DanielSurface.Alterity.com>
2019-12-17 17:14:28 -0600

    Refactored weather_station to accept command line argument "-t" for testing (test mode)
    When testing remotely without having the pi, you can use the "-t" parameter when starting weather_station.py. Test mode will call broadcast_info and pass random temp, baro and humidity values.

--- app/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    # Weather Underground
-    STATION_ID = "KTXFORTW595"
-    STATION_KEY = "wheaxkqp"
--- app/weather_station.py
+++ app/weather_station.py
@@ -1,143 +1,49 @@
 #!/usr/bin/python
-'''*****************************************************************************************************************
-    Pi Temperature Station
-    By John M. Wargo
-    www.johnwargo.com
-
-    This is a Raspberry Pi project that measures weather values (temperature, humidity and pressure) using
-    the Astro Pi Sense HAT then uploads the data to a Weather Underground weather station.
-********************************************************************************************************************'''
-
-from __future__ import print_function
 
 import datetime
 import os
 import sys
 import time
-from urllib import urlencode
-
-import urllib2
-from sense_hat import SenseHat
-
-from config import Config
-
 import json
 import requests
+import random
+from station import *
+from config import Config
 
-# ============================================================================
-# Constants
-# ============================================================================
-# specifies how often to measure values from the Sense HAT (in minutes)
 MEASUREMENT_INTERVAL = 10  # minutes
-# Set to False when testing the code and/or hardware
-# Set to True to enable upload of weather data to Weather Underground
 WEATHER_UPLOAD = False
-# the weather underground URL used to upload weather data
 WU_URL = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
-# some string constants
-SINGLE_HASH = "#"
-HASHES = "########################################"
-SLASH_N = "\n"
-SERVER_URL = "http://192.168.1.9:8080/update"
-# constants used to display an up and down arrows plus bars
-# modified from https://www.raspberrypi.org/learning/getting-started-with-the-sense-hat/worksheet/
-# set up the colours (blue, red, empty)
-b = [0, 0, 255]  # blue
-r = [255, 0, 0]  # red
-e = [0, 0, 0]  # empty
-# create images for up and down arrows
-arrow_up = [
-    e, e, e, r, r, e, e, e,
-    e, e, r, r, r, r, e, e,
-    e, r, e, r, r, e, r, e,
-    r, e, e, r, r, e, e, r,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e,
-    e, e, e, r, r, e, e, e
-]
-arrow_down = [
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    e, e, e, b, b, e, e, e,
-    b, e, e, b, b, e, e, b,
-    e, b, e, b, b, e, b, e,
-    e, e, b, b, b, b, e, e,
-    e, e, e, b, b, e, e, e
-]
-bars = [
-    e, e, e, e, e, e, e, e,
-    e, e, e, e, e, e, e, e,
-    r, r, r, r, r, r, r, r,
-    r, r, r, r, r, r, r, r,
-    b, b, b, b, b, b, b, b,
-    b, b, b, b, b, b, b, b,
-    e, e, e, e, e, e, e, e,
-    e, e, e, e, e, e, e, e
-]
 
+' support for python2 and python3 '
+try:
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urlencode
 
-def c_to_f(input_temp):
-    # convert input_temp from Celsius to Fahrenheit
-    return (input_temp * 1.8) + 32
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 
+try:
+    from sense_hat import SenseHat
+except ImportError:
+    print("SenseHat module not found.")
 
-def get_cpu_temp():
-    # 'borrowed' from https://www.raspberrypi.org/forums/viewtopic.php?f=104&t=111457
-    # executes a command at the OS to pull in the CPU temperature
-    res = os.popen('vcgencmd measure_temp').readline()
-    return float(res.replace("temp=", "").replace("'C\n", ""))
+def exec_test():
+    print("Initializing test station...")
+    test_station = Station()
+    # run test with sample values
+    while True:
+        sample_weather_data = {
+            "dateutc": "now",
+            "tempf": str(random.randint(32, 110)),
+            "humidity": str(random.randint(40, 99)),
+            "baromin": str(random.randint(1, 99)),
+        }
 
-
-# use moving average to smooth readings
-def get_smooth(x):
-    # do we have the t object?
-    if not hasattr(get_smooth, "t"):
-        # then create it
-        get_smooth.t = [x, x, x]
-    # manage the rolling previous values
-    get_smooth.t[2] = get_smooth.t[1]
-    get_smooth.t[1] = get_smooth.t[0]
-    get_smooth.t[0] = x
-    # average the three last temperatures
-    xs = (get_smooth.t[0] + get_smooth.t[1] + get_smooth.t[2]) / 3
-    return xs
-
-
-def get_temp():
-    # ====================================================================
-    # Unfortunately, getting an accurate temperature reading from the
-    # Sense HAT is improbable, see here:
-    # https://www.raspberrypi.org/forums/viewtopic.php?f=104&t=111457
-    # so we'll have to do some approximation of the actual temp
-    # taking CPU temp into account. The Pi foundation recommended
-    # using the following:
-    # http://yaab-arduino.blogspot.co.uk/2016/08/accurate-temperature-reading-sensehat.html
-    # ====================================================================
-    # First, get temp readings from both sensors
-    t1 = sense.get_temperature_from_humidity()
-    t2 = sense.get_temperature_from_pressure()
-    # t becomes the average of the temperatures from both sensors
-    t = (t1 + t2) / 2
-    # Now, grab the CPU temperature
-    t_cpu = get_cpu_temp()
-    # Calculate the 'real' temperature compensating for CPU heating
-    t_corr = t - ((t_cpu - t) / 1.5)
-    # Finally, average out that value across the last three readings
-    t_corr = get_smooth(t_corr)
-    # convoluted, right?
-    # Return the calculated temperature
-    return t_corr
-
-def broadcast_info(info):
-    try:
-        #upload_url = SERVER_URL + "?" + urlencode(info)
-        response = requests.post(SERVER_URL, data=info)
-        print(response.status_code, response.reason)
-        response.close()  # best practice to close the file
-    except:
-        print("Exception:", sys.exc_info()[0], SLASH_N)
+        test_station.broadcast_info(sample_weather_data)
+        time.sleep(5)   # sleep for 5 second intervals
 
 def main():
     global last_temp
@@ -221,9 +127,6 @@ def main():
                     # set last_temp to the current temperature before we measure again
                     last_temp = temp_f
 
-                    # ========================================================
-                    # Upload the weather data to Weather Underground
-                    # ========================================================
                     # is weather upload enabled (True)?
                     if WEATHER_UPLOAD:
                         # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
@@ -250,65 +153,29 @@ def main():
                     else:
                         print("Skipping Weather Underground upload")
 
-        # wait a second then check again
-        # You can always increase the sleep value below to check less often
         time.sleep(1)  # this should never happen since the above is an infinite loop
 
-    print("Leaving main()")
-
-
-# ============================================================================
-# here's where we start doing stuff
-# ============================================================================
-print(SLASH_N + HASHES)
-print(SINGLE_HASH, "Pi Weather Station                  ", SINGLE_HASH)
-print(SINGLE_HASH, "By John M. Wargo (www.johnwargo.com)", SINGLE_HASH)
-print(HASHES)
-
-# make sure we don't have a MEASUREMENT_INTERVAL > 60
 if (MEASUREMENT_INTERVAL is None) or (MEASUREMENT_INTERVAL > 60):
     print("The application's 'MEASUREMENT_INTERVAL' cannot be empty or greater than 60")
     sys.exit(1)
 
-# ============================================================================
-#  Read Weather Underground Configuration Parameters
-# ============================================================================
-print("\nInitializing Weather Underground configuration")
-wu_station_id = Config.STATION_ID
-wu_station_key = Config.STATION_KEY
-if (wu_station_id is None) or (wu_station_key is None):
-    print("Missing values from the Weather Underground configuration file\n")
-    sys.exit(1)
-
-# we made it this far, so it must have worked...
-print("Successfully read Weather Underground configuration values")
-print("Station ID:", wu_station_id)
-# print("Station key:", wu_station_key)
-
-# ============================================================================
-# initialize the Sense HAT object
-# ============================================================================
-try:
-    print("Initializing the Sense HAT client")
-    sense = SenseHat()
-    # sense.set_rotation(180)
-    # then write some text to the Sense HAT's 'screen'
-    sense.show_message("Init", text_colour=[255, 255, 0], back_colour=[0, 0, 255])
-    # clear the screen
-    sense.clear()
-    # get the current temp to use when checking the previous measurement
-    last_temp = round(c_to_f(get_temp()), 1)
-    print("Current temperature reading:", last_temp)
-except:
-    print("Unable to initialize the Sense HAT library:", sys.exc_info()[0])
-    sys.exit(1)
-
-print("Initialization complete!")
-
-# Now see what we're supposed to do next
+############
+#   MAIN   #
+############
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("\nExiting application\n")
-        sys.exit(0)
+    program_name = sys.argv[0]
+    parameter = sys.argv[1]
+    if parameter == None or parameter != "-t":
+        print("Invalid Usage! \nUsage: python weather_station.py params: [-t]")
+        print("Given: " + program_name + " " + parameter)
+        sys.exit(-1)
+    else:
+        print("Startup: " + program_name + " " + parameter )
+        if parameter == "-t":
+            exec_test()
+        else:
+            try:
+                main()
+            except KeyboardInterrupt:
+                print("\nExiting application\n")
+                sys.exit(0)
